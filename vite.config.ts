fb7de43e2a2d21812925d68ea54c458703ec7d21
Davit Vekua <davitvekua23@gmail.com>
2026-07-31 17:41:50 +0200

    Configure custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/cv-davitvekua/",
+  base: "/",
   plugins: [tailwindcss()],
   build: {
     rolldownOptions: {
